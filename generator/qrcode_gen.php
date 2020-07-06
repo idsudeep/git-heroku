b9963a977a97abbd493d09508db2155390c03ea9
idsudeep <sudeep.numb@gmail.com>
2020-07-06 20:18:05 +0530

    import clear0

--- generator/qrcode_gen.php
+++ generator/qrcode_gen.php
@@ -2,9 +2,9 @@
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<link rel="stylesheet" href="css/bootstrap.css">
-<script src="js/jquery-3.2.1.min.js"></script>
-<script src="js/bootstrap.min.js"></script>
+<link rel="stylesheet" href="../css/bootstrap.css">
+<script src="../js/jquery-3.2.1.min.js"></script>
+<script src="../js/bootstrap.min.js"></script>
 <!-- jQuery -->
 <title>Success</title>
 <link rel="stylesheet" href="css/style.css">
