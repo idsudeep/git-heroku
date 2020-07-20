db5bc6fb0109e320bd3e2852e3ff899cfb4db94e
idsudeep <sudeep.numb@gmail.com>
2020-07-20 15:35:34 +0530

    delete

--- generator/generate_code.php
+++ generator/generate_code.php
@@ -1,5 +1,5 @@
 <?php 
-error_reporting(E_ALL);
+error_reporting();
 
 if(isset($_POST) && !empty($_POST)) {
 	include('library/phpqrcode/qrlib.php'); 
--- generator/script/ajax_generate_code.js
+++ generator/script/ajax_generate_code.js
@@ -11,7 +11,7 @@ $(document).ready(function() {
                 
                 
                 
-                console.log(response);
+               
                 
 				$(".showQRCode").html(response); 
                 
