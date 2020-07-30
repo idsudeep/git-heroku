247d10fc6d75c3697ac23174494ba3de4c1deab7
idsudeep <sudeep.numb@gmail.com>
2020-07-30 21:49:12 +0530

    not risk

--- generator/generate_code.php
+++ generator/generate_code.php
@@ -1,11 +1,9 @@
 <?php 
-error_reporting();
-
-if(isset($_REQUEST) && !empty($_REQUEST)) {
+if(isset($_SERVER) && !empty($_SERVER)) {
 	include('library/phpqrcode/qrlib.php'); 
-	$codesDir = "codes/";	
+		
 	/*$codeFile = date('d-m-Y-h-i-s').'.png';*/
-	$codeContents = $_REQUEST['formData'];
+	$codeContents = $_POST['formData'];
 	
     
     $fileName = '005_file_'.($codeContents).'.png'; 
@@ -15,18 +13,21 @@ if(isset($_REQUEST) && !empty($_REQUEST)) {
     
     
      if (!file_exists($pngAbsoluteFilePath)) {
-         
+          /*  QRcode::png($codeContents, $pngAbsoluteFilePath); 
+         */
          QRcode::png($codeContents, $pngAbsoluteFilePath, $_POST['ecc'], $_POST['size']); 
             echo 'File generated!'; 
             echo '<hr />'; 
         } else { 
-            echo ' You can now Scan waiting to generate new One.!'; 
+            echo 'File already generated'; 
             echo '<hr />'; 
         } 
     
-
+    // generating QR code
+	
+	 // display generated QR code
 	echo '<img class="img-thumbnail" src="'.$pngAbsoluteFilePath.'" />';
-} 
-
-
+} else {
+	header('location:./');
+}
 ?>
